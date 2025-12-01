7d43298dc7a15815f9e587bd5baa9b602bc42430
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-12-01 18:47:38 +0530

    added product model

---
+++ src/models/product.models.js
@@ -0,0 +1,42 @@
+import moongoose from "moongoose"
+const productSchema = new moongoose.Schema({
+    productname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    productimage: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: true
+    },
+    stock: {
+        type: Number,
+        default: 0
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    owner: {
+        type: moongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+
+},
+    {
+        timestamps: true
+    }
+)

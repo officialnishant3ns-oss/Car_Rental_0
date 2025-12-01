812e2f6cea802d4e9e1ff69fc40f728e0caf11b7
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-12-01 18:56:44 +0530

    added index and app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 console.log("hello E-commerse")
+import dotenv from 'dotenv'
+dotenv.config(
+    {
+        path: './.env'
+    }
+)
+import app from './app.js'
 
+
+
+app.get('/api/v1', (req, res) => {
+    res.send("Server is working fine E-commerse")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is working on PORT ${process.env.PORT}`)
+})  

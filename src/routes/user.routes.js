904ace4621871b29897fa8319dada09c7915f185
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-01-29 17:05:02 +0530

    First commit Project setup

--- src/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-import User from "../models/user.models.js"
-import { registeruser } from "../controllers/user.controller.js"
-import express from "express"
-const router = express.Router()
-
-router.post("/register", registeruser)
-
-export default router

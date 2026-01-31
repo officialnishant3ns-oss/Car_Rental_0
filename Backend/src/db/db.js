2cfa5d53e86f2f6ace49f9a0e1d74a654518801c
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-01-31 23:25:25 +0530

    Basic correction in db and added folder for auth

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -4,7 +4,7 @@ import { DB_NAME } from '../constant.js'
 const connectDB = async (req, res) => {
     try {
         const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected Successfully:: DB_HOST: ${conection.connection.host}`)
+        console.log(`\n MongoDB connected Successfully:: DB_HOST: ${connection.connection.host}`)
 
     } catch (error) {
         console.error("Mongo_DB connection error:", error)
